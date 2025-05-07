0aa27170b57497dae830ae1375d6154411676a1e
fangxu <fangxu6@gmail.com>
2025-05-07 22:01:57 +0800

    fix: types/v3-directive not published

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="app">
+    <h1>你好啊!</h1>
+  </div>
+  <!-- html content -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script lnang="ts">
+// JS或TS
+export default {
+  name: 'App' // 组件名称
+}
+</script>
+
+<style>
+.app {
+  background-color: #f0f0f0;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+/* CSS */
 </style>
