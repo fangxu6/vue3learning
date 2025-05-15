544e653e932e12f4c3751b6e38176343afe97d02
fangxu <fangxu6@gmail.com>
2025-05-15 13:24:56 +0800

    feat(query参数): query参数查询

---
+++ 备份/37-route的query参数/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>Welcome to the home page!</p>
+  </div>
+</template>
+
+<script lang="ts" setup name="Home">
+
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
